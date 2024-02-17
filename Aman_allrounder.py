96f816d80fc84e609dcb26f5bed5c44bfc2e353f
amantiwari2091 <62899402+amantiwari2091@users.noreply.github.com>
2024-02-17 16:42:47 +0530

    rutba

--- Aman_allrounder.py
+++ Aman_allrounder.py
@@ -1,3 +1,7 @@
-a = int(input("Enter num a : "))
-b = int(input("Enter num b : "))
-print(f"{a} is greater than {b}" if(a>b) else f"{b} is greater than {a}")
+#a = int(input("Enter num a : "))
+#b = int(input("Enter num b : "))
+#print(f"{a} is greater than {b}" if(a>b) else f"{b} is greater than {a}")
+
+#Sir, question to pura suno
+
+print("a is greater than b")
